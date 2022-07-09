28afa09da665500c0906662fb73e2b394df90694
huzi-hub <h.t1149171@gmail.com>
2022-07-09 14:51:58 +0500

    first commit

---
+++ lib/screens/splashscreen.dart
@@ -0,0 +1,43 @@
+// ignore_for_file: camel_case_types
+// ignore_for_file: file_names
+
+import 'dart:async';
+
+import '/screens/home.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 3), () async {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const HomeScreen()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(82, 14, 125, 1),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(5),
+          decoration: BoxDecoration(
+              border: Border.all(color: Colors.white, width: 3),
+              borderRadius: BorderRadius.circular(5)),
+          child: const Text(
+            'ChatInUni',
+            style: TextStyle(fontSize: 30, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
